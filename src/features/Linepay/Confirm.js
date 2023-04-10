bc2d80f2350ecd6241b2ca8ca086aa6f6edf1bc1
yuchen <wameny10814@gmail.com>
2023-04-10 14:17:56 +0800

    230410 付款完成調整

--- src/features/Linepay/Confirm.js
+++ src/features/Linepay/Confirm.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Confirm() {
     return (
-        <div>
-            <h2>line Pay付款完成。期待您的下次光臨。</h2>
+        <div style={{textAlign:'center',height:'80vh',marginTop:'40vh'}}>
+            <h2>Line Pay付款完成!  期待您的下次光臨。</h2>
         </div>
     )
 }
